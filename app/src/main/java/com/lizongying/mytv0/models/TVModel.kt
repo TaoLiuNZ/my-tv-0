9a9a53502c726215b98e1996d9d0160b3c3d4d6d
TAO LIU <20410129+TaoLiuNZ@users.noreply.github.com>
2026-02-13 21:35:01 +1300

    Update TVModel.kt

--- app/src/main/java/com/lizongying/mytv0/models/TVModel.kt
+++ app/src/main/java/com/lizongying/mytv0/models/TVModel.kt
@@ -188,7 +188,9 @@ class TVModel(var tv: TV) : ViewModel() {
         val httpDataSource = _httpDataSource!!
 
         return when (getSourceTypeCurrent()) {
-            SourceType.HLS -> HlsMediaSource.Factory(httpDataSource).setAllowChunklessPreparation(false).createMediaSource(mediaItem)
+            SourceType.HLS -> HlsMediaSource.Factory(httpDataSource)
+               // .setAllowChunklessPreparation(false)
+                .createMediaSource(mediaItem)
             SourceType.RTSP -> if (userAgent.isEmpty()) {
                 RtspMediaSource.Factory().createMediaSource(mediaItem)
             } else {
